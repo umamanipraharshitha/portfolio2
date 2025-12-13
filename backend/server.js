f06c7b71b6918e6baaf0c5e5113ba830d42e1c0d
UMA MANI PRAHARSHITHA MANGIPUDI <166523910+umamanipraharshitha@users.noreply.github.com>
2025-12-13 20:09:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
+
 const cors = require("cors");
 const fs = require("fs/promises");
 const axios = require("axios"); // GitHub fetch
